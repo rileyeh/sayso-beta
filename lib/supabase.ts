050f50a30e1dbeac33a5266044578350296f7e00
tjwpatterson <tjwpatterson@gmail.com>
2025-10-27 23:24:02 -0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,13 @@
+// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
-)
+export const createSupabaseClient = () => {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+  if (!url || !key) {
+    throw new Error('Missing Supabase env vars')
+  }
+
+  return createClient(url, key)
+}
