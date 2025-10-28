402aa279b693da296bcd5db47df5bf06081c0a5e
tjwpatterson <tjwpatterson@gmail.com>
2025-10-27 21:02:08 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    TWILIO_SID: process.env.TWILIO_SID,
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_NUMBER: process.env.TWILIO_NUMBER,
+  },
+}
+
+module.exports = nextConfig
